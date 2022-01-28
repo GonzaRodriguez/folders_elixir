f81916d4eb2aff904f2af274117b34d69cf7b97f
Gonzalo Rodriguez <grodriguez1695@gmail.com>
2022-01-28 19:44:51 -0300

    tests for get_folders_by

--- prodeal_elixir/test/prodeal_elixir/folders_test.exs
+++ prodeal_elixir/test/prodeal_elixir/folders_test.exs
@@ -21,6 +21,28 @@ defmodule ProdealElixir.FoldersTest do
       folder = folder_fixture()
       assert Folders.get_folder!(folder.id) == folder
     end
+
+    test "get_folders_by/1 returns the folder with given item_name" do
+      %Folder{item_name: item_name} =
+        folder_to_be_filtered = folder_fixture(%{item_name: "filtering_test"})
+
+      filtered_folders = Folders.get_folders_by(item_name)
+
+      assert filtered_folders == [folder_to_be_filtered]
+      assert length(filtered_folders) == 1
+    end
+
+    test "get_folders_by/1 returns all folders with given item_name" do
+      item_name = "filtering_test"
+
+      Enum.each(0..5, fn _x ->
+        folder_fixture(%{item_name: item_name})
+      end)
+
+      filtered_folders = Folders.get_folders_by(item_name)
+
+      assert length(filtered_folders) == 6
+    end
   end
 
   describe "create folder" do
