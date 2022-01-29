839e0dad29c34f6c55579c6c71f5532d384273d4
Gonzalo Rodriguez <grodriguez1695@gmail.com>
2022-01-28 21:07:12 -0300

    add new plug to handle pagination params

---
+++ prodeal_elixir/lib/plugs/pagination_params.ex
@@ -0,0 +1,20 @@
+defmodule ProdealElixir.PaginationParams do
+  use ProdealElixirWeb, :params
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  embedded_schema do
+    field :page, :integer, default: 1
+    field :per_page, :integer, default: 2
+  end
+
+  @per_page_default 2
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:page, :per_page])
+    |> validate_number(:page, greater_than_or_equal_to: 1)
+    |> validate_number(:per_page, greater_than_or_equal_to: 1)
+    |> validate_number(:per_page, less_than_or_equal_to: 50)
+  end
+end
