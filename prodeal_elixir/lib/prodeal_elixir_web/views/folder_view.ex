eff8dda49c194a8074c7da2328a79ac992aac293
Gonzalo Rodriguez <grodriguez1695@gmail.com>
2022-01-28 21:00:59 -0300

    render pagination data

--- prodeal_elixir/lib/prodeal_elixir_web/views/folder_view.ex
+++ prodeal_elixir/lib/prodeal_elixir_web/views/folder_view.ex
@@ -2,8 +2,11 @@ defmodule ProdealElixirWeb.FolderView do
   use ProdealElixirWeb, :view
   alias ProdealElixirWeb.FolderView
 
-  def render("index.json", %{folders: folders}) do
-    %{data: render_many(folders, FolderView, "folder.json")}
+  def render("index.json", %{folders: folders, pagination_data: pagination_data}) do
+    %{
+      data: render_many(folders, FolderView, "folder.json"),
+      pagination_data: pagination_data
+    }
   end
 
   def render("folder.json", %{folder: folder}) do
